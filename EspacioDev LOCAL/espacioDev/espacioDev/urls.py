112f2d8627181a7b37acbf262574792ae677e789
Nicolas <gbarbacil91@gmail.com>
2020-08-19 21:59:50 -0300

    v0.0.1

--- EspacioDev LOCAL/espacioDev/espacioDev/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-# las vistas se declaran en cada app, y luego se importan como globales
-
-urlpatterns = [
-    path("", include("core.urls")),
-    path("admin/", admin.site.urls),
-]
